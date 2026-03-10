91c04f70035f7565a675a5459d25b842381b5104
MATTHEWPC\matth <matthewmaiden14@gmail.com>
2026-03-10 23:01:47 +0000

    - Created Codebase and File structure

---
+++ All-The-Beans.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace All_The_Beans_Project;
+
+public class IndexModel : PageModel
+{
+    public void OnGet() { }
+}
---
+++ All-The-Beans.Web/UI/ts/global.ts
@@ -0,0 +1,2 @@
+const message: string = "Hello TypeScript ts";
+console.log(message);
